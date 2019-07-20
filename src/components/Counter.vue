d03d296de62f18b32aa46418cc5d017a04a42523
homework <52529600+SKYE-633@users.noreply.github.com>
2019-07-20 18:29:06 +0800

    Add files via upload
    counter + -

---
+++ src/components/Counter.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+  <div class="hello">
+    <button v-on:click="counter += 1">+</button>
+    <span>{{ counter }}</span>
+    <button v-on:click="counter -= 1">-</button>
+  </div>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  data(){
+    return  {
+      counter:0
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/CounterGroup.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  
+  
+
+
+   
+
+</div>
+</template>
+
+<script>
+export default {
+   data(){
+     return {aaa:0}
+   }
+}
+</script>
+
